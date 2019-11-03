076cc1d0829e0032ebaffec4868fb357d1278148
Hendrik Wagemann <hendrik.wagemann@t-online.de>
2019-11-03 23:33:35 +0100

    added a test suite for all tests gradlew test now uses that test suite instead of a wildcard expression

---
+++ src/test/java/org/wahlzeit/AllTests.java
@@ -0,0 +1,20 @@
+package org.wahlzeit;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.wahlzeit.handlers.TellFriendTest;
+import org.wahlzeit.model.*;
+import org.wahlzeit.model.persistence.AbstractAdapterTest;
+import org.wahlzeit.model.persistence.DatastoreAdapterTest;
+import org.wahlzeit.services.EmailService;
+import org.wahlzeit.services.LogBuilderTest;
+import org.wahlzeit.utils.StringUtilTest;
+import org.wahlzeit.utils.VersionTest;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({TellFriendTest.class, DatastoreAdapterTest.class, AccessRightsTest.class, FlagReasonTest.class,
+        GenderTest.class, GuestTest.class, PhotoFilterTest.class, TagsTest.class, UserStatusTest.class, ValueTest.class,
+        EmailService.class, LogBuilderTest.class, StringUtilTest.class, VersionTest.class})
+public class AllTests
+{
+}
